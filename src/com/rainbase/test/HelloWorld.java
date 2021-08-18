f9b025d3edeefefbf7b1f73c646b600505ccc03c
caoguobin <caoguobin@live.com>
2021-08-18 19:56:55 +0800

    First push

---
+++ src/com/rainbase/test/HelloWorld.java
@@ -0,0 +1,14 @@
+/***********************************************
+ * File Name: HelloWorld
+ * Author: caoguobin
+ * mail: caoguobin@live.com
+ * Created Time: 18 08 2021 19:54
+ ***********************************************/
+
+package com.rainbase.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
